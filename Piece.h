e5fd08090b949624fbf091229625185c54ef2d0c
eyalSmarom <eyalsmarom@hotmail.com>
2016-12-12 21:51:18 +0200

    Update Piece.h

--- Piece.h
+++ Piece.h
@@ -1,1 +1,15 @@
+#pragma once
+#include <iostream>
 
+class Piece
+{
+	public:
+		Piece(char* type, char color, char currP[3]);
+		virtual int checkMove() const = 0;
+		virtual void move(char* action);
+
+	protected:
+		char* _type;
+		char _color;
+		char _currP[3];
+};
