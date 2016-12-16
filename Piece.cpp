9a10f9132764289d9bf5c0f02e71e5cdd862fad4
eyalSmarom <eyalsmarom@hotmail.com>
2016-12-16 15:45:39 +0200

    Update Piece.cpp

--- Piece.cpp
+++ Piece.cpp
@@ -1,14 +1,1 @@
-#include <iostream>
-#include "Piece.h"
 
-Piece::Piece(char* type, char color, char* currP)
-{
-	strcpy_s(this->_type, 6, type);
-	strcpy_s(this->_currP, 3, currP);
-	this->_color = color;
-}
-
-void Piece::updateCurrP(char* currP)
-{
-
-}
